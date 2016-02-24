bcb85bfe2d25a50f71ddc0e43f26b47004d554e0
prateeksan <sanyal.prateek@gmail.com>
2016-02-24 17:21:49 -0500

    added a proper hash

--- app.js
+++ app.js
@@ -13,14 +13,13 @@ message_log = [];
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  res.status(200).send(message_log[message_log.length - 1]);});
-
+  res.status(200).send(message_log);
+});
 app.listen(port, function(){
   console.log('Listening on port ' + port);
 });
 
 // building the bot
-
 app.post('/hello', function(req, res, next) {
   var userName = req.body.user_name;
   var botPayLoad = {
@@ -39,50 +38,52 @@ app.post('/reviewing', function(req, res, next) {
   var trigger = req.body.trigger_word;
   var userName = req.body.user_name;
   if(userName !== 'slackbot'){
-    var greeting = ""
+    var greeting = "";
     var entry = {};
-    var hello_pattern = new RegExp(/^hello/)
-    var reviewing_pattern = new RegExp(/^reviewing:/)
-    var notes_pattern = new RegExp(/^notes:/)
-    var score_pattern = new RegExp(/^score:/)
-    var end_pattern = new RegExp(/^end/)
+    var hello_pattern = new RegExp(/^hello/);
+    var reviewing_pattern = new RegExp(/^reviewing:/);
+    var notes_pattern = new RegExp(/^notes:/);
+    var score_pattern = new RegExp(/^score:/);
+    var end_pattern = new RegExp(/^end/);
+    var new_text = "";
 
-   
     if(text.test(reviewing_pattern)){
-      new_text = text.replace("reviewing:")
-        if(new_text[0] = " "){
-          new_text = new_text.substring(1);
-        };
+      new_text = text.replace("reviewing:");
+      if(new_text[0] == " "){
+        new_text = new_text.substring(1);
+      }
       entry['student'] = new_text; 
     }
     else if(text.test(notes_pattern)){
-      new_text = text.replace("notes:")
-        if(new_text[0] = " "){
-          new_text = new_text.substring(1);
-        };
+      new_text = text.replace("notes:");
+      if(new_text[0] == " "){
+        new_text = new_text.substring(1);
+      }
       entry['notes'] = new_text; 
     }
     else if(text.test(score_pattern)){
-      new_text = text.replace("score:")
-        if(new_text[0] = " "){
-          new_text = new_text.substring(1);
-        };
+      new_text = text.replace("score:");
+      if(new_text[0] == " "){
+        new_text = new_text.substring(1);
+      }
       entry['score'] = new_text; 
     }   
     else if(text.test(end_pattern)){
-       entry['reviewer'] = userName;
-       message_log.push(entry);
-    }
+     entry['reviewer'] = userName;
+     message_log = [];
+     message_log.push(entry);
+   }
 
-    if(text.test(hello_pattern)){
-      var botPayLoad = {
-        text: greeting
-      };
-    if(text.test(end_pattern)){
-      var botPayLoad = {
-        text: "Saved! Review by:" + userName + ". Text: " + text
-      };
-    }
+  if(text.test(hello_pattern)){
+    var botPayLoad = {
+      text: greeting
+    };
+  }
+  if(text.test(end_pattern)){
+    var botPayLoad = {
+      text: "Saved! Review by:" + userName + ". Text: " + text
+    };
+  }
     return res.status(200).json(botPayLoad);
   } 
   else {
