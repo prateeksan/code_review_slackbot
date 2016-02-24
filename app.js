0bd93269bcae525911f453ed21962c26b0043ad4
prateeksan <sanyal.prateek@gmail.com>
2016-02-24 14:19:36 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Setting up and testing server
+
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {res.status(200).send('hello world');});
+
+app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
+
+// building the bot
+
+app.post('/hello', function(req, res, next) {
+  var userName = req.body.user_name;
+  var botPayLoad = {
+    text: 'Hello' + username + ', welcome to the lhl Slack channel!'
+  };
+
+  if(userName)
+});
