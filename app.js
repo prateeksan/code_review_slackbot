50324a2671207241507da36dc4851c978b2d0a5c
prateeksan <sanyal.prateek@gmail.com>
2016-02-24 16:54:10 -0500

    added a proper hash

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ message_log = [];
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  res.status(200).send(message_log);});
+  res.status(200).send(message_log[message_log.length - 1]);});
 
 app.listen(port, function(){
   console.log('Listening on port ' + port);
@@ -38,17 +38,54 @@ app.post('/reviewing', function(req, res, next) {
   var text = req.body.text;
   var trigger = req.body.trigger_word;
   var userName = req.body.user_name;
-  var entry = {};
-  entry['reviewer'] = userName;
-  entry['notes'] = text;
-  var botPayLoad = {
-    text: "Saved! Review by:" + userName + ". Text: " + text + trigger
-  };
-
   if(userName !== 'slackbot'){
-    message_log.push(entry);
+    var greeting = ""
+    var entry = {};
+    var hello_pattern = new RegExp(/^hello/)
+    var reviewing_pattern = new RegExp(/^reviewing:/)
+    var notes_pattern = new RegExp(/^notes:/)
+    var score_pattern = new RegExp(/^score:/)
+    var end_pattern = new RegExp(/^end/)
+
+   
+    if(text.test(reviewing_pattern)){
+      new_text = text.replace("reviewing:")
+        if(new_text[0] = " "){
+          new_text = new_text.substring(1);
+        };
+      entry['student'] = new_text; 
+    }
+    else if(text.test(notes_pattern)){
+      new_text = text.replace("notes:")
+        if(new_text[0] = " "){
+          new_text = new_text.substring(1);
+        };
+      entry['notes'] = new_text; 
+    }
+    else if(text.test(score_pattern)){
+      new_text = text.replace("score:")
+        if(new_text[0] = " "){
+          new_text = new_text.substring(1);
+        };
+      entry['score'] = new_text; 
+    }   
+    else if(text.test(end_pattern)){
+       entry['reviewer'] = userName;
+       message_log.push(entry);
+    }
+
+    if(text.test(hello_pattern)){
+      var botPayLoad = {
+        text: greeting
+      };
+    if(text.test(end_pattern)){
+      var botPayLoad = {
+        text: "Saved! Review by:" + userName + ". Text: " + text
+      };
+    }
     return res.status(200).json(botPayLoad);
-  } else {
+  } 
+  else {
     return res.status(200).end();
   }
 });
